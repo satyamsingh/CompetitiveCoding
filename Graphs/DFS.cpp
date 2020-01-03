18a1336e5306737ae7b0274d9a4242f4be3ad243
satyamsingh <f2015707@pilani.bits-pilani.ac.in>
2020-01-03 23:14:55 +0530

    DFS

---
+++ Graphs/DFS.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+  int V;
+  list<int> *adj;
+  //Why this * is important
+
+public:
+  Graph(int V);
+  void addEdge(int u, int v);
+  void DFS(int s);
+  void DFSUtil(int v, bool visited[]);
+};
+
+Graph::Graph(int V)
+{
+  this->V = V;
+  adj = new list<int>[V];
+}
+
+void Graph::addEdge(int u, int v)
+{
+  adj[u].push_back(v);
+}
+
+void Graph::DFSUtil(int v, bool visited[])
+{
+    // Mark the current node as visited and
+    // print it
+    visited[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices adjacent
+    // to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFSUtil(*i, visited);
+}
+
+// DFS traversal of the vertices reachable from v.
+// It uses recursive DFSUtil()
+void Graph::DFS(int v)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Call the recursive helper function
+    // to print DFS traversal
+    DFSUtil(v, visited);
+}
+
+// Driver code
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Depth First Traversal"
+            " (starting from vertex 2) \n";
+    g.DFS(2);
+
+    return 0;
+}
